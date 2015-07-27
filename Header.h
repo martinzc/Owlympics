24bb2c8ff834f4fc1e356523278acfdf4f231a0c
Martin Zhou <cz16@rice.edu>
2015-07-27 10:57:05 +0800

    Add initial configurations: Import libz.dylib / gimbal framework Add NSAlwaysInUsage notification Initialize header file

---
+++ Header.h
@@ -0,0 +1,15 @@
+//
+//  Header.h
+//  owlympics
+//
+//  Created by Martin Zhou on 7/27/15.
+//  Copyright (c) 2015 Martin Zhou. All rights reserved.
+//
+
+#ifndef owlympics_Header_h
+#define owlympics_Header_h
+
+
+#endif
+
+#import <Gimbal/Gimbal.h>
