d06065af8668277f2615d220219027c930b8eb57
Martin Zhou <cz16@rice.edu>
2015-08-19 11:52:24 -0700

    Finished table view

--- owlympics/TodayViewController.swift
+++ owlympics/TodayViewController.swift
@@ -10,6 +10,61 @@ import UIKit
 
 class TodayViewController: UIViewController, UITableViewDataSource, GMBLCommunicationManagerDelegate, GMBLPlaceManagerDelegate {
     
+    @IBOutlet weak var graphView: GraphView!
+    // label outlets
+    
+    @IBOutlet weak var AverageTime: UILabel!
+    
+    @IBOutlet weak var maxLabel: UILabel!
+    
+    func setupGraphDisplay() {
+        
+        //Use 7 days for graph - can use any number,
+        //but labels and sample data are set up for 7 days
+        let noOfDays:Int = 7
+        
+        //1 - replace last day with today's actual data
+//        graphView.graphPoints[graphView.graphPoints.count-1] = counterView.counter
+        
+        //2 - indicate that the graph needs to be redrawn
+        graphView.setNeedsDisplay()
+        
+        maxLabel.text = "\(maxElement(graphView.graphPoints))"
+        
+        //3 - calculate average from graphPoints
+        let average = graphView.graphPoints.reduce(0, combine: +)
+            / graphView.graphPoints.count
+        AverageTime.text = "\(average)"
+        
+        //set up labels
+        //day of week labels are set up in storyboard with tags
+        //today is last day of the array need to go backwards
+        
+        //4 - get today's day number
+        let dateFormatter = NSDateFormatter()
+        let calendar = NSCalendar.currentCalendar()
+        let componentOptions:NSCalendarUnit = .CalendarUnitWeekday
+        let components = calendar.components(componentOptions,
+            fromDate: NSDate())
+        var weekday = components.weekday
+        
+        let days = ["S", "S", "M", "T", "W", "TH", "F"]
+        
+        //5 - set up the day name labels with correct day
+        for i in reverse(1...days.count) {
+            if let labelView = graphView.viewWithTag(i) as? UILabel {
+                if weekday == 7 {
+                    weekday = 0
+                }
+                labelView.text = days[weekday--]
+                if weekday < 0 {
+                    weekday = days.count - 1
+                }
+            }
+        }
+    }
+    
+    
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         return 1
     }
@@ -43,6 +98,7 @@ class TodayViewController: UIViewController, UITableViewDataSource, GMBLCommunic
         
         placeManager = GMBLPlaceManager()
         placeManager.delegate = self
+        setupGraphDisplay()
     }
     
     func placeManager(manager: GMBLPlaceManager!, didBeginVisit visit: GMBLVisit!) {
